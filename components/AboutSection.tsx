f087190cd5bb75e48e41e038b1709c8cee054b7e
naughty8020 <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2026-03-08 15:26:23 +0900

    fix(main):AboutSetionText

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -7,13 +7,11 @@ export default function AboutSection() {
       <div className="max-w-7xl mx-auto lg:max-w-screen-2xl mx-auto text-center">
         <h2 className="font-shippori text-md  lg:text-3xl font-bold mb-4">私たちについて</h2>
         <p className="font-shippori text-sm lg:text-2xl text-gray-700 space-y-4">
-          私たちサンプル会社は、革新と創造性に突き動かされています。
-          <br />
-          私たちの使命は、企業を力強くし、人々に感動を与えるソリューションをデザインすることです。
-          <br />
-          最先端の技術と洗練されたデザインを組み合わせ、価値ある体験を提供します。
-          <br />
-          ビジョナリーなチームによって設立されたサンプル会社は、常に可能性の限界に挑戦し、アイデアを現実に変え続けています。
+          滋賀県野洲市に拠点を置き、車両販売から整備、車検、レッカーなど全て自社で完結できる環境がございます。 
+<br />
+　愛車のお探しからメンテナス、緊急時のトラブルも全て対応させて頂き、お客様と愛車を全力でサポートいたします。 
+
+　 
         </p>
       </div>
 
