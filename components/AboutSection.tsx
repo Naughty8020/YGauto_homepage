3565fa2bdf0867234dc0c7f8a346c4b85c9f4058
naughty8020 <169634290+Naughty8020@users.noreply.github.com>
2026-03-13 16:45:06 +0900

    del(Hero):image

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -3,13 +3,17 @@
 
 export default function AboutSection() {
   return (
-    <section className="py-15 px-10 ">
+    <section className="mt-50 py-15 px-10 ">
       <div className="max-w-7xl mx-auto lg:max-w-screen-2xl mx-auto text-center">
         <h2 className="font-shippori text-md  lg:text-3xl font-bold mb-4">私たちについて</h2>
-        <p className="font-shippori text-sm lg:text-2xl text-gray-700 space-y-4">
-          滋賀県野洲市に拠点を置き、車両販売から整備、車検、レッカーなど全て自社で完結できる環境がございます。 
+        <p className="font-shippori text-sm lg:text-2xl text-gray-700 space-y-4 px-50">
+          滋賀県野洲市に拠点を置き、車両販売から整備、車検、レッカーなど
+          <br />
+          全て自社で完結できる環境がございます。 
 <br />
-　愛車のお探しからメンテナス、緊急時のトラブルも全て対応させて頂き、お客様と愛車を全力でサポートいたします。 
+　愛車のお探しからメンテナス、緊急時のトラブルも全て対応させて頂き、
+<br />
+          お客様と愛車を全力でサポートいたします。 
 
 　 
         </p>
