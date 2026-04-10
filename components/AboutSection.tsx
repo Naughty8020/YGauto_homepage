88ceaba1bce097bcdd7540ecba314e98895b749f
naughty8020 <169634290+Naughty8020@users.noreply.github.com>
2026-04-10 16:18:08 +0900

    fix(About)add mt

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,6 +1,6 @@
 export default function AboutSection() {
   return (
-    <section className="mt-30 md:mt-24 lg:mt-45 py-10 md:py-16 px-4 sm:px-6 lg:px-8">
+    <section className="mt-20 md:mt-24 lg:mt-35 py-10 md:py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-5xl mx-auto text-center">
         <h2 className=" text-3xl sm:text-3xl md:text-3xl lg:text-4xl font-bold lg:mt-5 mb-6">
           私たちについて
